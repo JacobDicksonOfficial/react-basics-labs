fea696f654651334109c63ecd442b7a9a0d53bb9
Jacob <jjacobdofficial@gmail.com>
2024-10-04 15:07:06 +0100

    Tasky Lab 2 - Excercises done

--- tasky/src/components/Task.js
+++ tasky/src/components/Task.js
@@ -1,17 +1,21 @@
 import React from 'react';
 
 const Task = (props) => {
-    
+    // Dynamically assign class based on priority level
+    const priorityClass = `priority-${props.priority.toLowerCase()}`;
+
     return (
-        <div className="card" style={{backgroundColor: props.done ? 'lightgrey' : '#5bb4c4'}}>
+        <div className="card">
             <p className="title">{props.title}</p>
             <p>Due: {props.deadline}</p>
             {props.description && <p>{props.description}</p>}
-            <p>Priority: {props.priority}</p> 
+            {/* Apply the priority class */}
+            <p className={priorityClass}>{props.priority}</p>
             <button onClick={props.markDone} className='doneButton'>Done</button>
-            <button className='deleteButton' onClick={props.deleteTask}>Delete</button> 
+            <button className='deleteButton' onClick={props.deleteTask}>Delete</button>
         </div>
-    )
+    );
 }
 
 export default Task;
+
